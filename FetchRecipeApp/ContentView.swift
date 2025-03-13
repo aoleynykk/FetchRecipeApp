613f4b146608a79e50d978b8fedbe71d4b939681
aoleynykk <olliynukk@gmail.com>
2025-03-13 13:14:50 +0200

    init

---
+++ FetchRecipeApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FetchRecipeApp
+//
+//  Created by Alex Oliynyk on 13.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Олександр Олійник on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
